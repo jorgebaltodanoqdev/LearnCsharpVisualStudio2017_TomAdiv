76a81cb392f2000ebd9a895284af2552c76fd222
Jorge Baltodano Quispe <34632982+jorgebaltodanoq@users.noreply.github.com>
2020-08-26 19:31:54 -0500

    @jorgebaltodanoqdev
    Control Flujo: Continuando

--- Lesson02_ControlFlujo/Program.cs
+++ Lesson02_ControlFlujo/Program.cs
@@ -13,13 +13,13 @@ namespace Lesson02_ControlFlujo
             // da como respuesta un boleano
             WriteLine("COMPARADORES");
 
-            WriteLine($"{4} =  {5}? : {4 == 5}"); // Comparas si 4 = 5 -> No o false
-            WriteLine($"{4} <  {5}? : {4 <  5}"); //  4  es menor que 5 -> yes or true
-            WriteLine($"{4} <= {5}? : {4 <= 5}");  //4  es menor o igual que 5 -> yes or true
+            WriteLine($"{4} =  ? : {4 == 5}"); // Comparas si 4 = 5 -> No o false
+            WriteLine($"{4} <  ? : {4 <  5}"); //  4  es menor que 5 -> yes or true
+            WriteLine($"{4} <= ? : {4 <= 5}");  //4  es menor o igual que 5 -> yes or true
             WriteLine($"{4} <= {4}? : {4 <= 4}");  //4  es menor o igual que 4 -> yes or true
-            WriteLine($"{4} >  {5}? : {4 >  5}");  //4  es meyor 5 -> not or false
-            WriteLine($"{5} >= {5}? : {5 >= 5}");  //5  es meyor o igual 5 -> yes or true
-            WriteLine($"{4} != {5}? : {4 != 5} \n");  //4  es diferente 5 -> yes or true
+            WriteLine($"{4} >  ? : {4 >  5}");  //4  es meyor 5 -> not or false
+            WriteLine($" >= ? : {5 >= 5}");  //5  es meyor o igual 5 -> yes or true
+            WriteLine($"{4} != ? : {4 != 5} \n");  //4  es diferente 5 -> yes or true
 
 
             // 2. Aprendiendo If and Else para hacer una 
@@ -30,8 +30,6 @@ namespace Lesson02_ControlFlujo
             decimal precioone = 500M;
             decimal preciotwo = 100M;
 
-            //Utilizando If and else
-
             if (precioone > preciotwo) //precioone es mayor  preciotwo si es true se ejecuta la sentencia
                 WriteLine($"EL precio {precioone:C} es mayor {preciotwo:C}");
             else
@@ -41,6 +39,58 @@ namespace Lesson02_ControlFlujo
                 WriteLine($"EL precio {precioone:C} es menor {preciotwo:C}");
             else
                 WriteLine($"EL precio {precioone:C} es mayor {preciotwo:C}");
+
+
+            // USANDO MULTIPLES IF AND ELSE
+            WriteLine("\nMULTIPLES IF AND ELSE  ");
+
+            int valorX = 500;
+            int valorY = 100;
+
+            //Validar 500<100; 100>500; 500=500
+
+            if (valorY < valorX)
+                WriteLine($"El valor {valorX} < {valorY}");
+            else if(valorX > valorY)
+                WriteLine($"El valor {valorY} > {valorX}");
+            else if(valorX==valorY)
+                WriteLine($"El valor {valorX} = {valorY}");
+
+
+            // USANDO TRYPARSE PARA SER MAS LEGIBLE EN EL CODIGO
+
+            WriteLine("\n USANDO TRYPARSE");
+
+            WriteLine("Ingrese un valor:");
+            string valor = ReadLine();
+            if (double.TryParse(valor, out double value))
+                WriteLine($"{value} * 0.50 ={value * 0.50}");
+            else
+                WriteLine($"{valor} * 0.50= No es factible..");
+
+
+            // USANDO OPERADORES ++ ; -- ; =+ ; =- 
+            WriteLine("\n USANDO OPERADORES INCREMENTO Y DECREMENTO");
+
+            int numero = 1;
+            
+            // Da el mismo resultado ya que ++ incrementa en 1
+            WriteLine($" {numero} + 1  : {numero + 1}"); // 2
+            WriteLine($" ++numero     : { ++numero }");   //  2
+
+            // Da el mismo resultado ya que -- decrementa en 1
+            WriteLine($" {numero} - 1  : {numero - 1}"); // 2
+            WriteLine($" --numero     : { --numero }");   //  2
+
+            // Tiene la misma funcionalidad en incremento
+            WriteLine($" numero = numero + 20 => numero ={numero + 20}"); 
+            WriteLine($" numero  += 20 => {numero+=20}");
+
+            // Tiene la misma funcionalidad en decremento
+            WriteLine($" numero = numero - 20 => numero ={numero - 20}");
+            WriteLine($" numero  -= 20 => {numero -= 20}");
+
+
 
             // Permiti que la ventana de consola no se cierre.
             WriteLine("\nEnter close");
